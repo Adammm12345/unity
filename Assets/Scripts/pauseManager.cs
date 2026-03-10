bad18892bb50c44246a35457f0c08373239f5809
Adammm12345 <adamalalouche@gmail.com>
2026-03-10 11:13:07 +0100

    Ajout des différentes animations

---
+++ Assets/Scripts/pauseManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool isGamePaused = false;
+
+        [SerializeField]
+    private GameObject pauseMenuUI;
+ 
+ 
+    private void Awake()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isGamePaused = !isGamePaused;
+
+            if (isGamePaused == true)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
